4771df03e88085224b6aa4557379cc49c1fda35b
Andrej Zerox <kitanovicandrej213@gmail.com>
2022-08-18 19:43:51 +0200

    Logging cbc reponse

--- src/controllers/webhook.ts
+++ src/controllers/webhook.ts
@@ -47,6 +47,7 @@ export const postWebhookCustomer: RequestHandler = async (req, res, next) => {
 			},
 		};
 		const cbcResponse = await cbcPostCreditReport(cbcApplicant);
+		console.log(cbcResponse)
 		const jsonResponse = JSON.parse(xmlToJson.toJson(cbcResponse.data));
 		const htmlReport = jsonResponse.XML_INTERFACE.CREDITREPORT.REPORT;
 
