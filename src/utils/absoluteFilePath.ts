89d924201bfdbd3854e82343090abfc9fbcd9470
Andrej Zerox <kitanovicandrej213@gmail.com>
2022-11-18 00:33:39 +0100

    feat: added https

--- src/utils/absoluteFilePath.ts
+++ src/utils/absoluteFilePath.ts
@@ -2,5 +2,5 @@ import { Request } from 'express';
 import path from 'path';
 
 export const absoluteFilePath = (req: Request, filePath: string) => {
-	return path.join(req.headers.host ?? '', filePath);
+	return 'https://' + path.join(req.headers.host ?? '', filePath);
 };
