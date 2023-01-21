73b88fe7a1a0c39537568eedeb834c24a9d33f05
Andrej Zerox <kitanovicandrej213@gmail.com>
2023-01-21 20:30:15 +0100

    feat: added roles

--- src/routes/hubspot.ts
+++ src/routes/hubspot.ts
@@ -10,7 +10,7 @@ defineRoutes(router, [
 	{
 		method: 'get',
 		route: '/lenders',
-		// roles: ['user', 'admin'],
+		roles: ['user', 'admin'],
 		// permissions: ['read:distillation'],
 		controller: getHubspotLendersController,
 	},
