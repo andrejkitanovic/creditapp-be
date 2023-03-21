5f3e4773acf3fc45319b4e53aecdf593c92f2293
Andrej Zerox <kitanovicandrej213@gmail.com>
2023-03-21 18:13:14 -0400

    feat: added timezone to dayjs

--- src/utils/dayjs.ts
+++ src/utils/dayjs.ts
@@ -1,9 +1,16 @@
 import dayjs from 'dayjs';
+import timezone from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
 
-export const startOfYear = (year: number) => dayjs().set('year', year).startOf('year');
-export const endOfYear = (year: number) => dayjs().set('year', year).endOf('year');
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+dayjs.tz.setDefault('Iceland');
+
+export const startOfYear = (year: number) => dayjs.tz(dayjs()).set('year', year).startOf('year');
+export const endOfYear = (year: number) => dayjs.tz(dayjs()).set('year', year).endOf('year');
 export const dayjsUnix = (date: number | string | undefined) => {
-	if (!date) return dayjs();
+	if (!date) return dayjs.tz(dayjs());
 
-	return dayjs(parseInt(date as string));
+	return dayjs.tz(parseInt(date as string));
 };
