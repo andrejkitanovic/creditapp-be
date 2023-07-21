32b0b3d1d2ebdbad1b6421c0f15ccf005c2b9196
Andrej Zerox <kitanovicandrej213@gmail.com>
2023-07-21 20:54:58 +0200

    feat: added create user as partner admin

--- src/controllers/organisation.ts
+++ src/controllers/organisation.ts
@@ -3,8 +3,10 @@ import { queryFilter } from 'helpers/filters';
 
 import i18n from 'helpers/i18n';
 import { createMeta } from 'helpers/meta';
+import User from 'models/user';
 import Organisation from 'models/organisation';
 import { hsCreateLeadSource, hsCreateUser, hsGetUserByEmail, hsUpdatePartnerTable } from './hubspot';
+import { sendEmailInvitation } from 'utils/mailer';
 
 export const getMineOrganisation: RequestHandler = async (req, res, next) => {
 	try {
@@ -63,6 +65,15 @@ export const postOrganisation: RequestHandler = async (req, res, next) => {
 
 		// Update Partner Table
 		await hsUpdatePartnerTable(organisation);
+
+		// Create User as Partner Admin
+		const user = await User.create({
+			organisation: organisation._id,
+			hubspotId: hsUser?.id,
+			email: email.toLowerCase(),
+			role: 'partner-admin',
+		});
+		await sendEmailInvitation({ userId: user._id, email });
 
 		res.json({
 			message: i18n.__('CONTROLLER.ORGANISATION.POST_ORGANISATION.ADDED'),
